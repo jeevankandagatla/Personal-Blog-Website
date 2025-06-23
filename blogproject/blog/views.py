348aab3bc2c0ac37508b22836de2e55bf90ef3ef
Jeevan Kandagatla <jeevankandakatla@gmail.com>
2025-06-23 18:09:04 +0530

    Added logon, signup pages, and forms to the blog project.

--- blogproject/blog/views.py
+++ blogproject/blog/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Post
+from .forms import PostForm
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def post_list(request):
@@ -10,3 +12,18 @@ def post_list(request):
 def post_detail(request, pk):
     post = Post.objects.get(pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
+
+@login_required
+def post_create(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.save()
+            return render(request, 'blog/post_detail.html', {'post': post})
+        else:
+            form = PostForm()
+    else:
+        return render(request, 'blog/post_create.html', {'form': form})
+    
--- blogproject/blogproject/urls.py
+++ blogproject/blogproject/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),  # Include blog app URLs
+    path('accounts/', include('django.contrib.auth.urls')),  # Include authentication URLs
 ]
