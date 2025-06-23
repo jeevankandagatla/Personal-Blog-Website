68a4d20a3d129b5db10e0e0ee99c696989229bcb
Jeevan Kandagatla <jeevankandakatla@gmail.com>
2025-06-23 17:06:32 +0530

    Added blog views, templates and URLs

--- blogproject/blog/views.py
+++ blogproject/blog/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Post
 
 # Create your views here.
+def post_list(request):
+    posts = Post.objects.all().order_by('-created_at')
+    return render(request, 'blog/post_list.html', {'posts': posts})
+
+def post_detail(request, pk):
+    post = Post.objects.get(pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
