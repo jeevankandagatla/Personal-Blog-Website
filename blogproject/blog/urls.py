0b9408db797122283b0162b0bf741cf1ba785a8a
Jeevan Kandagatla <jeevankandakatla@gmail.com>
2025-07-23 03:58:07 +0530

    Fix login and signup page linking

--- blogproject/blog/urls.py
+++ blogproject/blog/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from . import views
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
     path('signup/', views.signup, name='signup'),
     path('login/', views.login_view, name='login'),
+    path('logout/', LogoutView.as_view(next_page='index'), name='logout'),
 ]
